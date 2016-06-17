56c7b5b094013a8708c8870a373350d490b0d739
Elie Zananiri <prisonerjohn@gmail.com>
2016-06-17 17:44:50 -0400

    Updated template to use programmable pipeline!

--- Templates/SceneTemplate/src/main.cpp
+++ Templates/SceneTemplate/src/main.cpp
@@ -2,12 +2,13 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+int main()
+{
+	ofGLWindowSettings settings;
+	settings.setGLVersion(4, 1);
+	settings.width = 1920;
+	settings.height = 1080;
+	ofCreateWindow(settings);
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
 	ofRunApp(new ofApp());
-
 }
