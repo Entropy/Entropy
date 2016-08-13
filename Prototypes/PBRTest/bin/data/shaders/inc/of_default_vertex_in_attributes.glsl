2380608a40a830332e4d2a0dfd098ebedec3e0ea
Elie Zananiri <prisonerjohn@gmail.com>
2016-08-13 17:07:34 +0200

    Cleaned out Prototypes folder.

--- Prototypes/PBRTest/bin/data/shaders/inc/of_default_vertex_in_attributes.glsl
+++
@@ -1,7 +0,0 @@
-#ifndef DEFAULT_VERTEX_IN_ATTRIBUTES
-#define DEFAULT_VERTEX_IN_ATTRIBUTES
-layout( location = 0 ) in vec4  position;
-layout( location = 1 ) in vec4  color;
-layout( location = 2 ) in vec3  normal;
-layout( location = 3 ) in vec2  texcoord;
-#endif // DEFAULT_VERTEX_IN_ATTRIBUTES
--- Prototypes/ParticleSystem/bin/data/shaders/inc/of_default_uniforms.glsl
+++
@@ -1,8 +0,0 @@
-#ifndef OF_DEFAULT_UNIFORMS
-#define OF_DEFAULT_UNIFORMS
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelViewMatrix;
-uniform mat4 modelViewProjectionMatrix;
-uniform mat4 textureMatrix;
-#endif // OF_DEFAULT_UNIFORMS
