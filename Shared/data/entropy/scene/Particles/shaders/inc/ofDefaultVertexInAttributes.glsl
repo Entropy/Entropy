70b19b24a25243d1e470426ebbdeacf3fc6c2cf7
Elie Zananiri <prisonerjohn@gmail.com>
2016-08-13 17:12:11 +0200

    Deleted stray data files.

--- Shared/data/entropy/scene/Particles/shaders/inc/ofDefaultVertexInAttributes.glsl
+++
@@ -1,9 +0,0 @@
-#ifndef OF_DEFAULT_VERTEX_IN_ATTRIBUTES
-#define OF_DEFAULT_VERTEX_IN_ATTRIBUTES
-
-layout( location = 0 ) in vec4 position;
-layout( location = 1 ) in vec4 color;
-layout( location = 2 ) in vec3 normal;
-layout( location = 3 ) in vec2 texcoord;
-
-#endif // OF_DEFAULT_VERTEX_IN_ATTRIBUTES
