d77566a4bb6e536c3cec1d706b1d16ee5d6453ed
Elie Zananiri <prisonerjohn@gmail.com>
2017-05-16 11:41:54 -0400

    Updated Particles scene to pre-rendered standalone.

--- Projects/SceneParticles/src/ofApp.h
+++ Projects/SceneParticles/src/ofApp.h
@@ -1,6 +1,16 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxTextureRecorder.h"
+#include "ofxTimeline.h"
+
+#include "entropy/Helpers.h"
+#include "entropy/particles/ParticleSystem.h"
+#include "entropy/particles/Photons.h"
+#include "entropy/particles/Environment.h"
+#include "entropy/render/PostEffects.h"
+#include "entropy/render/WireframeFillRenderer.h"
 
 class ofApp 
 	: public ofBaseApp
@@ -23,4 +33,87 @@ public:
 	void windowResized(int w, int h);
 	void dragEvent(ofDragInfo dragInfo);
 	void gotMessage(ofMessage msg);
+
+public:
+	//bool saveState(const string & path);
+	//bool loadState(const string & path);
+
+	bool loadPreset(const string & presetName);
+	bool savePreset(const string & presetName);
+
+	void reset();
+
+	static const string kSceneName;
+	static const float kHalfDim;
+	static const unsigned int kMaxLights;
+
+	ofEasyCam camera;
+
+	nm::ParticleSystem particleSystem;
+	nm::Photons photons;
+	nm::Environment::Ptr environment;
+
+	ofShader shader;
+	ofShader::TransformFeedbackSettings shaderSettings;
+	ofBufferObject feedbackBuffer;
+	ofVbo feedbackVbo;
+	GLuint numPrimitives, numPrimitivesQuery;
+	std::vector<ofLight> pointLights;
+
+	entropy::render::WireframeFillRenderer renderer;
+	entropy::render::PostEffects postEffects;
+	entropy::render::PostParameters postParams;
+	ofFbo fboScene;
+	ofFbo fboPost;
+
+	vector<ofEventListener> eventListeners;
+	ofxPanel gui;
+	ofxTimeline timeline;
+	ofxTLCameraTrack cameraTrack;
+
+	ofxTextureRecorder textureRecorder;
+
+	std::string currPreset;
+
+	struct : ofParameterGroup
+	{
+		ofParameter<void> resetScene{ "Reset Scene" };
+		ofParameter<bool> debugLights{ "Debug Lights", false };
+
+		//ofParameter<string> stateFile;
+
+		struct : ofParameterGroup
+		{
+			ofParameter<bool> colorsPerType{ "Color per Type", true };
+			ofParameter<bool> additiveBlending{ "Additive Blend", true };
+			ofParameter<bool> drawPhotons{ "Draw Photons", true };
+			ofParameter<float> ambientLight{ "Ambient Light", 0.001, 0, 0.02 };
+			ofParameter<float> attenuation{ "Attenuation", 0.01, 0.0000001, 0.05 };
+			ofParameter<float> lightStrength{ "Light Strength", 1, 0, 1 };
+
+			PARAM_DECLARE("Rendering",
+				colorsPerType,
+				additiveBlending,
+				drawPhotons,
+				ambientLight,
+				attenuation);
+		} rendering;
+
+		struct : ofParameterGroup
+		{
+			ofParameter<bool> recordSequence{ "Record Sequence", false };
+			ofParameter<bool> recordVideo{ "Record Video", false };
+
+			PARAM_DECLARE("Recording",
+				recordSequence,
+				recordVideo);
+		} recording;
+
+		PARAM_DECLARE("Particles",
+			resetScene,
+			debugLights,
+			//stateFile,
+			rendering,
+			recording);
+	} parameters;
 };
